2a3af0ae8e1a4b735b553d650111675ed815e421
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-27 16:00:24 +0100

    add time formatting

--- wetter/Current.swift
+++ wetter/Current.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Current {
     
-    var currentTime: Int
+    var currentTime: String?
     var temperature: Int
     var humidity: Double
     var precipProbability: Double
@@ -20,11 +20,23 @@ struct Current {
     init(weatherDictionary: NSDictionary) {
         let currentWeather = weatherDictionary["currently"] as NSDictionary
         
-        currentTime = currentWeather["time"] as Int
         temperature = currentWeather["temperature"] as Int
         humidity = currentWeather["humidity"] as Double
         precipProbability = currentWeather["precipProbability"] as Double
         summary = currentWeather["summary"] as String
         icon = currentWeather["icon"] as String
+        
+        let currentTimeIntValue = currentWeather["time"] as Int
+        currentTime = dateStringFromUnixtime(currentTimeIntValue)
+    }
+    
+    func dateStringFromUnixtime(unixtime: Int) -> String {
+        let timeInSeconds = NSTimeInterval(unixtime)
+        let weatherDate = NSDate(timeIntervalSince1970: timeInSeconds)
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = .ShortStyle
+        
+        return dateFormatter.stringFromDate(weatherDate)
+        
     }
 }
